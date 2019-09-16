d9f5a8c4edd3bdf5a441edd1eeaa5d6cbdfa3df3
Andrey Filippov <afi@i-loto.ru>
2019-09-16 15:32:18 +0300

    removed useless files

--- get-container-ip.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' php7)
-
-echo http://$IP:9191
--- run-in-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ISRUNNING=$(docker inspect -f '{{.State.Running}}' php7)
-
-if [[ "$ISRUNNING" == "true" ]]; then
-	docker exec -it php7 bash -c "$@"
-else
-	docker run --rm --name php7 -v $(pwd):/app php7-afi-alpine bash -c "$@"
-fi
